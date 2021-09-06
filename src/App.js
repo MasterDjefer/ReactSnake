5ba5ca1852cc9470de7373870c3710f4e82f59c2
MasterDjefer <kostia210@gmail.com>
2021-09-06 13:53:05 +0300

    init commit(made running snake)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Direction = 
+{ 
+  left:  "left", 
+  right: "right",
+  up:  "up",
+  down: "down",
+};
+
+class App extends React.Component 
+{
+  constructor()
+  {
+    super();
+
+    this.canvasRef = React.createRef();
+
+    this.state =
+    {
+      body: [ { x: 0, y: 0 } ],
+      direction: Direction.right,
+      blockSize: 20      
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot)
+  {
+    console.log(this.state.body.length);
+
+    const { body, blockSize } = this.state;
+    const ctx = this.canvasRef.current.getContext("2d");
+    prevState.body.forEach(element => ctx.clearRect(element.x, element.y, blockSize, blockSize));
+    body.forEach(element => ctx.fillRect(element.x, element.y, blockSize, blockSize));
+  }
+
+  componentDidMount()
+  {
+    document.addEventListener("keydown", this.onKeyPressed, false);
+
+    let self = this;
+    setInterval(() =>
+    {
+      let { body, blockSize } = this.state;
+      const cBody = JSON.parse(JSON.stringify(body));
+
+      const head = cBody[0];
+      for (let i = cBody.length - 1; i > 0; --i)
+      {
+        cBody[i].x = cBody[i - 1].x;
+        cBody[i].y = cBody[i - 1].y;
+      }
+
+      switch (this.state.direction)
+      {
+        case Direction.left:
+          head.x -= blockSize;
+          break;
+        case Direction.right:
+          head.x += blockSize;
+          break;
+        case Direction.up:
+          head.y -= blockSize;
+          break;
+        case Direction.down:
+          head.y += blockSize;
+          break;
+        default:
+          break;
+      }
+      
+      self.setState({ body: cBody });
+    }, 100);
+  }
+
+  onKeyPressed = (event) =>
+  {
+    let { body, direction } = this.state; 
+    body = JSON.parse(JSON.stringify(body));
+    
+    switch (event.keyCode)
+    {
+      case 65://A, left
+        if (this.state.direction !== Direction.right)
+          direction = Direction.left
+        break;
+      case 68://D, right
+        if (this.state.direction !== Direction.left)
+          direction = Direction.right
+        break;
+      case 87://W, up
+        if (this.state.direction !== Direction.down)
+          direction = Direction.up
+        break;
+      case 83://S, down
+        if (this.state.direction !== Direction.up)
+          direction = Direction.down
+        break;
+      case 32:        
+        body.push({ x: body[body.length - 1].x, y: body[body.length - 1].y });
+        break;
+      default:
+        break;
+    }
+      
+    this.setState({ body, direction });
+  }
+
+  render()
+  {
+    return (
+      <div className="app">
+        <canvas width="500" height="500" ref={this.canvasRef}/>
+      </div>
+    );
+  }
 }
 
 export default App;
